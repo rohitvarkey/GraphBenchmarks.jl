b2d23ea4ef1567d95c26bf9fd2d86c7f3eb881d1
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2017-03-03 02:06:42 -0500

    Added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,15 @@ using GraphBenchmarks
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+const benchmarkspecs = (StingerBenchmarks(), Graph500())
+for benchmarkspec in benchmarkspecs
+    info("Running $benchmarkspec benchmarks")
+    info("Running SerialBFS for StingerGraph")
+    @test isa(benchmark(benchmarkspec, StingerGraph, SerialBFS(), Kronecker(10, 16)), BenchmarkTools.Trial)
+    info("Running LevelSynchronousBFS for StingerGraph")
+    @test isa(benchmark(benchmarkspec, StingerGraph, LevelSynchronousBFS(), Kronecker(10, 16)), BenchmarkTools.Trial)
+    info("Running BFS for LightGraphs Graph")
+    @test isa(benchmark(benchmarkspec, LGGraph{LightGraphs.Graph}, SerialBFS(), Kronecker(10, 16)), BenchmarkTools.Trial)
+    info("Running BFS for LightGraphs DiGraph")
+    @test isa(benchmark(benchmarkspec, LGGraph{LightGraphs.DiGraph}, SerialBFS(), Kronecker(10, 16)), BenchmarkTools.Trial)
+end
